f7f8e801a3f8763e3d9102ea26cbea1032f37438
XueqiangChen <569503960@qq.com>
2020-10-14 11:05:57 +0800

    leetcode-102:SymmetricTree

--- src/main/java/leetcode/editor/cn/SymmetricTree.java
+++ src/main/java/leetcode/editor/cn/SymmetricTree.java
@@ -49,8 +49,35 @@ public class SymmetricTree {
      * }
      */
     class Solution {
+        /**
+         * 两个树互为镜像，需要满足两个条件：
+         * 它们的两个根结点具有相同的值
+         * 每个树的右子树都与另一个树的左子树镜像对称
+         * @param root
+         * @return
+         */
         public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
 
+            return isMirror(root.left, root.right);
+        }
+
+        private boolean isMirror(TreeNode node1, TreeNode node2) {
+            if (node1 == null && node2 == null) {
+                return true;
+            }
+
+            if (node1 == null || node2 == null) {
+                return false;
+            }
+
+            if (node1.val != node2.val) {
+                return false;
+            }
+
+            return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
